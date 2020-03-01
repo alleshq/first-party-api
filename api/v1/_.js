d769772951c99d0aab38f46a138d26b38ff0f74c
Archie Baer <archie@abaer.dev>
2020-03-01 14:03:17 +0000

    Session auth just for /me

--- api/v1/_.js
+++ api/v1/_.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const applicationAuth = require("../../util/applicationAuth");
-const sessionAuth = require("../../util/sessionAuth");
 
 const router = express.Router();
 router.use(applicationAuth);
-router.get("/me", sessionAuth, require("./me"));
+router.get("/me", require("./me"));
 router.get("/user", require("./user"));
 
 module.exports = router;
--- api/v1/me.js
+++ api/v1/me.js
@@ -1,16 +1,31 @@
-module.exports = async (req, res) => {
-    res.json({
-        id: req.user.id,
-        username: req.user.username,
-        name: req.user.name,
-        nickname: req.user.nickname,
-        reputation: req.user.reputation,
-        rubies: req.user.rubies,
-        private: req.user.private,
-        about: req.user.about,
-        plus: req.user.plus,
-        createdAt: req.user.createdAt,
-        primaryId: req.user.primaryId,
-        followers: await req.user.countFollowers()
+const db = require("../../util/db");
+const credentials = require("../../credentials");
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+
+    //Parse Header
+    const authToken = req.query.session;
+    if (typeof authToken !== "string") return res.status(400).json({err: "invalidSession"});
+    var token;
+    try {
+        token = jwt.verify(authToken, credentials.jwtSecret);
+    } catch (err) {
+        return res.status(400).json({err: "invalidSession"});
+    }
+
+    //Get Session
+    const session = await db.Session.findOne({
+        where: {
+            id: token.session
+        },
+        include: ["user"]
     });
+    if (!session) return res.status(400).json({err: "invalidSession"});
+
+    //Response
+    res.json({
+        id: session.user.id
+    });
+
 };
